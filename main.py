ed77aa562c54c74e62bfa9cb3307e5c412ff3aac
l9l9n-HP <l9l9ndos@gmail.com>
2026-03-06 01:46:14 +0600

    count event user

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from loads_csv import load_csv
-from metric import count_total_events
+from metric import count_total_events, count_events_user
 
 
 events = load_csv('events-1.csv')
 total_events = count_total_events(events)
+user_events = count_events_user(events)
 
-print(f"events: {total_events}")
+# print(f"events: {total_events}")
+# print(f"events user: {user_events}")
 
 
 if __name__ == "__main__":
--- metric.py
+++ metric.py
@@ -1,6 +1,17 @@
 from functools import reduce
+from collections import defaultdict
 
 
 def count_total_events(events):
     # считает общее количество событий в списке events
     return reduce(lambda acc, event: acc + 1, events, 0)
+
+
+def count_events_user(events):
+    # кол-во событий для каждого пользователя
+    user_events = defaultdict(int)
+    for event in events:
+        user = event['user']
+        user_events[user] += 1
+    return dict(user_events)
+
